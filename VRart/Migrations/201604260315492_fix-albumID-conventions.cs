65dde8776b4d9cb78fb1542251d4f37bcc8bba6c
Konbonix <ecrobertson@gmail.com>
2016-05-02 13:18:19 -0600

    migrations

---
+++ VRart/Migrations/201604260315492_fix-albumID-conventions.cs
@@ -0,0 +1,16 @@
+namespace VRart.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fixalbumIDconventions : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
